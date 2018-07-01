65ce8e8184f9df61302e945aac8bb219de7de5aa
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2018-07-01 00:36:10 -0400

    Fix chromium video scaling

--- js/main.js
+++ js/main.js
@@ -1,31 +1,27 @@
 var trueWidth = -1,
     trueHeight = -1;
-$('document').ready(function() {
-    let screen = document.getElementById('screen');
-    let ctx = screen.getContext('2d');
-    ctx.imageSmoothingEnabled = false;
-    ctx.mozImageSmoothingEnabled = false;
-    ctx.oImageSmoothingEnabled = false;
-    ctx.webkitImageSmoothingEnabled = false;
-    ctx.msImageSmoothingEnabled = false;
-    ctx.filter = 'saturate(130%)'; // A little bit of saturation won't hurt (experimental feature so some stuff might not see this but its not important)
 
-    //screen.height = 160;
-    //screen.width = 240;
-    //screen.height *= 3;
-    //screen.width *= 3;
-var connection = new WebSocket('ws://localhost:' + prompt("Asio sucks", "8080"));
-connection.binaryType = "arraybuffer";
-connection.onopen = function() {
-    console.log('Connection opened');
-    connection.send("8.username,4.Fork;");
-    connection.send("7.connect,4.emu1;");
-};
 
-connection.onmessage = function(event) {
-    // Binary message type
-    if(event.data instanceof ArrayBuffer) {
-        let bytearray = new Uint8Array(event.data);
+// Object for storing callbacks (exposed to allow for userscripts to extend functionality)
+var app = {
+    chat: {
+        log: function(who, message) {
+            let chat_list = document.getElementById('chat-list-items');
+            let message_container = document.createElement('div');
+            message_container.className = 'chat-item';
+            message_container.innerHTML = `<p class="username">` + who + `</p><p class="separator">:</p><span class="chat-text">` + message + `</span>`
+            chat_list.appendChild(message_container);
+            chat_list.scrollTop = chat_list.scrollHeight;
+        }
+    },
+    onchat: function(command) {
+        let message = command[2].replace(/(\\x[\da-f]{2}|\\u[\da-f]{4}|\\u{1[\da-f]{4}})+/g, function a(x) {
+                return eval("'" + x + "'")
+        });
+        app.chat.log(command[1], command[2]);
+    },
+    onscreen: function(imgdata) {
+        let bytearray = new Uint8Array(imgdata);
         var binstr = Array.prototype.map.call(bytearray, function (ch) {
             return String.fromCharCode(ch);
         }).join('');
@@ -40,10 +36,15 @@ connection.onmessage = function(event) {
                     trueHeight = image.height;
 
                     let aspectRatio = image.width / image.height;
-                    let maxCanvasHeight = Math.floor(window.getComputedStyle(document.getElementById('display')).height.slice(0, -2));
+                    // Better ways to do this, but Google doesn't know how to implement a standard...
+                    let oneRem = parseFloat(getComputedStyle(document.documentElement).fontSize);
+                    let maxCanvasHeight = parseFloat(window.getComputedStyle(document.getElementsByClassName('display-container')[0]).height) - (2 * oneRem);
 
                     let newWidth = aspectRatio * maxCanvasHeight;
                     let newHeight = maxCanvasHeight;
+
+                    app.chat.log("[Debug]", "New width: " + newWidth);
+                    app.chat.log("[Debug]", "New height: " + newHeight);
 
                     canvas.width = newWidth;
                     canvas.height = newHeight;
@@ -57,19 +58,106 @@ connection.onmessage = function(event) {
             }
         });
         image.src = 'data:image/jpeg;base64,' + b64encoded;
-
-    } else { // Plaintext message type
-        console.log('text message: ' + event.data);
+    },
+    onlist: function(command) {
+        app.chat.log("[Server]", "Users online: " + (command.length - 1));
     }
 };
 
-connection.onclose = function() {
-    console.log('Connection closed.');
-};
+var connection;
 
-connection.onerror = function() {
-    console.log('Connection error.');
+function decodeCommand(string) {
+	var pos = -1
+	var sections = []
+    for (let i =0;i < 25;++i) {
+		var len = string.indexOf('.', pos + 1)
+        if (len == -1) {
+			break
+		}
+		pos = parseInt(string.slice(pos + 1, len)) + len + 1
+		sections.push(string.slice(len + 1, pos)
+			.replace(/&#x27;/g, "'")
+			.replace(/&quot;/g, '"')
+			.replace(/&#x2F;/g, '/')
+			.replace(/&lt;/g, '<')
+			.replace(/&gt;/g, '>')
+			.replace(/&amp;/g, '&')
+		)
+		if (string.slice(pos, pos + 1) == ';') {
+			break
+		}
+	}
+	return sections
 }
+
+function encodeCommand(cypher) {
+	var command = "";
+	for (var i = 0; i < cypher.length; i++) {
+		var current = cypher[i];
+		command += current.length + "." + current;
+		command += (i < cypher.length - 1 ? "," : ";");
+	}
+	return command;
+}
+
+$('document').ready(function() {
+    let screen = document.getElementById('screen');
+    let ctx = screen.getContext('2d');
+    ctx.imageSmoothingEnabled = false;
+    ctx.mozImageSmoothingEnabled = false;
+    ctx.oImageSmoothingEnabled = false;
+    ctx.webkitImageSmoothingEnabled = false;
+    ctx.msImageSmoothingEnabled = false;
+    ctx.filter = 'saturate(130%)'; // A little bit of saturation won't hurt (experimental feature so some stuff might not see this but its not important)
+
+    connection = new WebSocket('ws://localhost:' + prompt("Asio sucks", "8080"));
+    connection.binaryType = "arraybuffer";
+    connection.onopen = function() {
+        console.log('Connection opened');
+        connection.send("8.username,4.Fork;");
+        connection.send("7.connect,4.emu1;");
+    };
+
+    connection.onmessage = function(event) {
+        // Binary message type
+        if(event.data instanceof ArrayBuffer) {
+            app.onscreen(event.data);
+        } else { // Plaintext message type
+            console.log('text message: ' + event.data);
+            let command = decodeCommand(event.data);
+            if(command.length == 0)
+                return;
+
+            switch(command[0]) {
+                case "chat":
+                    app.onchat(command);
+                    break;
+                case "list":
+                    app.onlist(command);
+                    break;
+                default:
+                    console.log("Unimplemented command: " + command[0]);
+            }
+
+        }
+    };
+
+    connection.onclose = function() {
+        console.log('Connection closed.');
+    };
+
+    connection.onerror = function() {
+        console.log('Connection error.');
+    }
+
+    document.getElementById('chat-input-box').onkeyup = function(e) {
+        if(e.keyCode == 13 && !e.shiftKey) {
+            e.preventDefault();
+            let message = document.getElementById('chat-input-box').value.slice(0, -1);
+            connection.send("4.chat," + message.length + '.' + message + ';');
+            document.getElementById('chat-input-box').value = "";
+        }
+    }
 });
 
 function drawSMPTEBars(canvas, ctx) {
