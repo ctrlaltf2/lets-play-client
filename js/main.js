6cf34e48492a682791e8dff3e7aa40c72c090f41
ctrlaltf2 <ctrlaltf2@protonmail.ch>
2018-07-01 13:00:44 -0400

    Retab

--- js/main.js
+++ js/main.js
@@ -80,37 +80,37 @@ var app = {
 var connection;
 
 function decodeCommand(string) {
-	var pos = -1
-	var sections = []
+    var pos = -1
+    var sections = []
     for (let i =0;i < 25;++i) {
-		var len = string.indexOf('.', pos + 1)
+        var len = string.indexOf('.', pos + 1)
         if (len == -1) {
-			break
-		}
-		pos = parseInt(string.slice(pos + 1, len)) + len + 1
-		sections.push(string.slice(len + 1, pos)
-			.replace(/&#x27;/g, "'")
-			.replace(/&quot;/g, '"')
-			.replace(/&#x2F;/g, '/')
-			.replace(/&lt;/g, '<')
-			.replace(/&gt;/g, '>')
-			.replace(/&amp;/g, '&')
-		)
-		if (string.slice(pos, pos + 1) == ';') {
-			break
-		}
-	}
-	return sections
+            break
+        }
+        pos = parseInt(string.slice(pos + 1, len)) + len + 1
+        sections.push(string.slice(len + 1, pos)
+            .replace(/&#x27;/g, "'")
+            .replace(/&quot;/g, '"')
+            .replace(/&#x2F;/g, '/')
+            .replace(/&lt;/g, '<')
+            .replace(/&gt;/g, '>')
+            .replace(/&amp;/g, '&')
+        )
+        if (string.slice(pos, pos + 1) == ';') {
+            break
+        }
+    }
+    return sections
 }
 
 function encodeCommand(cypher) {
-	var command = "";
-	for (var i = 0; i < cypher.length; i++) {
-		var current = cypher[i];
-		command += current.length + "." + current;
-		command += (i < cypher.length - 1 ? "," : ";");
-	}
-	return command;
+    var command = "";
+    for (var i = 0; i < cypher.length; i++) {
+        var current = cypher[i];
+        command += current.length + "." + current;
+        command += (i < cypher.length - 1 ? "," : ";");
+    }
+    return command;
 }
 
 $('document').ready(function() {
