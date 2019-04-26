63da9e6f4885bd6a8348d18698293b7e95b2ace6
ctrlatlf2 <ctrlaltf2@protonmail.ch>
2019-04-26 19:21:41 -0400

    Autoconnect to the client based on the url
    Now that client and server are merged, no need for setting a specific ip to onnect to

--- src/js/Main.js
+++ src/js/Main.js
@@ -3,7 +3,7 @@ import LetsPlaySocket from './LetsPlaySocket.js'
 
 $('document').ready(function() {
     var client = new LetsPlayClient();
-    var connection = new LetsPlaySocket('ws://' + prompt('Dev server uri', window.location.hostname + ':3074'), client);
+    var connection = new LetsPlaySocket('ws://' + window.location.host, client);
     client.updateSocket(connection);
 
     // Expose most of the application for userscripts to extend and for debugging
