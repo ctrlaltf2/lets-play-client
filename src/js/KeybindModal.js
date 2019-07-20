04cb277a1b6871943002ea8b3bcbbf3de5883694
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2019-07-20 09:13:24 -0400

    Allow analog button (joysticks, triggers, etc.) to be assigned to buttons

--- src/js/KeybindModal.js
+++ src/js/KeybindModal.js
@@ -10,10 +10,13 @@ function KeybindModal(client) {
     this.configuringButton = undefined;
 
     this.unsavedLayout;
-    //              { Button event
-    // Controller --
-    //              { Axes event
-    // Keyboard
+
+    /*
+     * Pool that stores gamepad events so that they can be batch-processed.
+     * Batch processing allows us to detect physical controller anomalies and figure out whether
+     * the user actualy meant to press a button/change an axes, and act accordingly.
+     */
+    this.gamepadEventPool = [];
 
     // On open, listen for events from controller or keyboard
     // called from modal open code in LetsPlayClient
@@ -25,6 +28,7 @@ function KeybindModal(client) {
     this.stopListen = function() {
         self.configuringDevice = self.configuringButton = self.unsavedLayout = undefined;
         self.listening = false;
+        self.gamepadEventPool = [];
     }
 
     this.saveLayout = function() {
@@ -32,18 +36,127 @@ function KeybindModal(client) {
         client.gamepadManager.setLayout(self.configuringDevice, self.unsavedLayout);
     }
 
-    window.addEventListener('gamepadButtonPress', function(evt) {
+    function selectKeybinding(event) {
+        /*
+        if(gamepad.axes[i] !== state.axes[i]) {
+            events.push({
+                player: index,
+                id:     gamepad.id,
+                button: {
+                    type: 'axes',
+                    id: i,
+                    value: {
+                        old: state.axes[i],
+                        new: gamepad.axes[i]
+                    }
+                }
+            });*/
+        let id = event.button.id,
+            type = event.button.type,
+            value = event.button.value,
+            retroID = RetroJoypad.indexOf(self.configuringButton);
+
+        console.log(self.configuringButton);
+        console.log('selected event: ', event);
+        // Remove other instances of this retroID being assigned
+        self.unsavedLayout.button = self.unsavedLayout.button.map((e, i) => {
+            if(e === null || e === retroID) {
+                console.log("button conflict for", e, ' at index', i);
+                return null;
+            }
+
+            return e;
+        });
+
+        // Then analog
+        self.unsavedLayout.axes = self.unsavedLayout.axes.map((p,i) => {
+            if(p === null) {
+                console.log('axes conflict for', p, ' at index', i);
+                return [];
+            }
+
+            return [p[0] === retroID ? null : p[0], p[1] === retroID ? null : p[1]];
+        });
+
+
+        /*
+        // Change text display
+        var buttonText = type + ' ' + id;
+
+        if(type === 'axes')
+            buttonText += value.new > 0 ? '+' : '-';
+
+        buttonText = buttonText.charAt(0).toUpperCase() + buttonText.slice(1);
+
+        $('#' + self.configuringButton).text(buttonText);
+
+        // Update localStorage keybindings
+        let buttonName = type === 'button' ?
+            id + '' :
+            id + (value.new > 0 ? '+' : '-');*/
+
+        if(type === 'button')
+            self.unsavedLayout[type][id] = retroID;
+        else if(type === 'axes') {
+            if(self.unsavedLayout[type][id] == undefined)
+                self.unsavedLayout[type][id] = [];
+            if(value.new > 0)
+                self.unsavedLayout[type][id][1] = retroID;
+            else
+                self.unsavedLayout[type][id][0] = retroID;
+        }
+
+        client.displayBindings();
+
+        self.configuringButton = undefined;
+    }
+
+    function updateKeybinds() {
+        console.log('gamepadPool: ', self.gamepadEventPool)
+        var events = self.gamepadEventPool.filter(e => { // Filter button release
+            return e.button.type === 'axes' || Math.abs(e.button.value.new) === 32767;
+        }).filter(e => { // Filter floatey joysticks
+            return e.button.type === 'button'  || Math.abs(e.button.value.new) > 0.25 * 32767;
+        });
+        console.log('events: ', events);
+
+        self.gamepadEventPool = [];
+
+        // We might have cleared out all of the events because of the above filters
+        if(events.length === 0)
+            return;
+
+        let buttonPresses = 0, axesPresses = 0;
+        events.forEach(event => {
+            if(event.button.type === 'button')
+                ++buttonPresses;
+            else if(event.button.type === 'axes')
+                ++axesPresses;
+        });
+
+        // Special case: pressing a single button on the controller shows up on the computer as an axes AND button update
+        if(buttonPresses >= 1 && axesPresses >= 1) {
+            selectKeybinding(events.find(e => e.button.type === 'button')); // Select first button
+            return;
+        }
+
+        // Otherwise select the first event that got there
+        selectKeybinding(events[0]);
+    }
+
+    window.addEventListener('gamepadEvent', function(evt) {
         if(self.listening === false)
             return;
 
         if(self.configuringDevice === undefined) { // Waiting for a button press to select device
             self.configuringDevice = evt.detail.id;
 
-            // Tell client to update the modal and pull layout for that device and display it
-            client.displayBindings(evt.detail.id);
-
             // Update unsaved layout
             self.unsavedLayout = client.gamepadManager.getLayout(evt.detail.id);
+
+            // Tell client to update the modal and pull layout for that device and display it
+            client.displayBindings();
+
             return;
         }
 
@@ -51,15 +164,17 @@ function KeybindModal(client) {
             return;
 
         if(self.configuringButton !== undefined) { // Waiting for a button press to configure key
-            // Change text display
-            $('#' + self.configuringButton).text('Button ' + evt.detail.button);
-            // Update localStorage keybindings
-            for(let i in self.unsavedLayout.buttons) {
-                if(self.unsavedLayout.buttons[i].name === self.configuringButton) {
-                    self.unsavedLayout.buttons[i].deviceValue = evt.detail.button;
-                }
-            }
-            self.configuringButton = undefined;
+            // Push event
+            self.gamepadEventPool.push(evt.detail);
+
+            /*
+             * If that event was brand new, schedule a flush. Successive calls to this function
+             * would realistically be only able to be called because of a floaty joystick or a
+             * controller which sends an axes AND button change for one physical press.
+             */
+            if(self.gamepadEventPool.length === 1)
+                setTimeout(updateKeybinds, 100);
+
         }
     });
 
@@ -69,11 +184,13 @@ function KeybindModal(client) {
 
         if(self.configuringDevice === undefined) { // Waiting for a button press
             self.configuringDevice = 'keyboard';
-            // Tell client to display keybindings
-            client.displayBindings('keyboard');
 
             // Update unsaved layout
             self.unsavedLayout = client.gamepadManager.getLayout('keyboard');
+
+            // Tell client to display keybindings
+            client.displayBindings();
+
             return;
         }
 
@@ -81,15 +198,20 @@ function KeybindModal(client) {
             return;
 
         if(self.configuringButton !== undefined) {
-            $('#' + self.configuringButton).text(evt.key || evt.keyCode || evt.which);
-            // Update localStorage keybindings
-            for(let i in self.unsavedLayout.buttons) {
-                if(self.unsavedLayout.buttons[i].name === self.configuringButton) {
-                    self.unsavedLayout.buttons[i].deviceValue = evt.key || evt.keyCode || evt.which;
-                }
-            }
+            let key = evt.key;
+            //$('#' + self.configuringButton).text(key);
+            // Remove other instances for this key
+            Object.keys(self.unsavedLayout.button).forEach(k => {
+                if(k === key)
+                    delete self.unsavedLayout.button[k];
+            });
+
+            self.unsavedLayout.button[key] = RetroJoypad.indexOf(self.configuringButton);
             self.configuringButton = undefined;
+
+            client.displayBindings();
         }
+
     });
 }
 
